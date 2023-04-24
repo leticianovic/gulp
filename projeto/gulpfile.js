233e83611d390a3d86db2d34e18123b44d901f2a
Letícia Silva <leticia.silva_2013@hotmail.com>
2023-04-24 15:33:53 -0300

    Gulp II - NodeJs: Visão geral

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,12 +1,16 @@
+const {series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const imagemin = require('gulp-imagemin')
+const stripJs = require('gulp-strip-comments')
+const stripCss = require('gulp-strip-css-comments')
+const htmlmin = require('gulp-htmlmin')
 
-function tarefasCSS(cb){
-    return gulp.src([
+function tarefasCSS(callback){
+    gulp.src([
         './node_modules/bootstrap/dist/css/bootstrap.css',
         './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
         './vendor/owl/dist/assets/owl.carousel.css',
@@ -17,10 +21,12 @@ function tarefasCSS(cb){
         .pipe(cssmin()) // minifica css
         .pipe(rename({suffix: '.min'})) // styles.min.css
         .pipe(gulp.dest('./dist/css')) // cria arquivo em novo diretorio
+
+    return callback()
 }
 
-function tarefasJS(){
-    return gulp.src([
+function tarefasJS(callback){
+    gulp.src([
         './node_modules/jquery/dist/jquery.js',
         './node_modules/bootstrap/dist/js/bootstrap.js',
         './vendor/owl/dist/owl.carousel.js',
@@ -32,6 +38,8 @@ function tarefasJS(){
         .pipe(uglify()) // minifica js
         .pipe(rename({suffix: '.min'})) // scripts.min.js
         .pipe(gulp.dest('./dist/js')) // cria arquivo em novo diretorio
+
+    return callback()
 }
 
 function tarefasImagem(){
@@ -50,6 +58,17 @@ function tarefasImagem(){
     .pipe(gulp.dest('./dist/images'))
 }
 
+// POC - Proof of Concept
+function tarefasHTML(callback){
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()
+}
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+exports.default = parallel(tarefasHTML, tarefasJS, tarefasCSS)
